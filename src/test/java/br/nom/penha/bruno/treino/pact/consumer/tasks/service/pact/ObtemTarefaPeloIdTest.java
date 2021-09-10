f42ddc6a614ae5a9b7be4241664dc3f39f021aef
Bruno C. Penha <dev@bruno.penha.nom.br>
2021-09-10 13:18:42 -0300

    Ajuste no teste para validar uma data como String

---
+++ src/test/java/br/nom/penha/bruno/treino/pact/consumer/tasks/service/pact/ObtemTarefaPeloIdTest.java
@@ -0,0 +1,59 @@
+package br.nom.penha.bruno.treino.pact.consumer.tasks.service.pact;
+
+import au.com.dius.pact.consumer.dsl.DslPart;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit.PactProviderRule;
+import au.com.dius.pact.consumer.junit.PactVerification;
+import au.com.dius.pact.core.model.RequestResponsePact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import br.nom.penha.bruno.treino.pact.consumer.tasks.model.Task;
+import br.nom.penha.bruno.treino.pact.consumer.tasks.service.TasksConsumer;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class ObtemTarefaPeloIdTest {
+
+    @Rule
+    public PactProviderRule mockProvider = new PactProviderRule("Tarefas", this);
+
+    @Pact(consumer = "ConsumidorBasico")
+    public RequestResponsePact criaPacto(PactDslWithProvider construtor){
+        DslPart corpo = new PactDslJsonBody()
+                            .numberType("id",1L)
+                            .stringType("task")
+                            .stringType("dueDate");
+
+        return construtor
+                .given("Existe uma tarefa com o id = 1")
+                .uponReceiving("Quando vier uma tarefa #1")
+                    .path("/todo/1")
+                    .method("GET")
+                .willRespondWith()
+                    .status(200)
+                    .body(corpo)
+                .toPact();
+    }
+
+    @Test
+    @PactVerification
+    public void test() throws IOException {
+        // Preparo o ambiente
+        TasksConsumer consumidor = new TasksConsumer(mockProvider.getUrl());
+        System.out.println("mockProvider.getUrl() >>>>>>>>>>> " + mockProvider.getUrl());
+
+        // Executo a tarefa que quero testar
+        Task tarefa = consumidor.getTask(1L);
+
+        // Verifico o resultado
+        assertThat(tarefa.getId(), is(1L));
+        assertThat(tarefa.getTask(), is(notNullValue()));
+
+    }
+}
--- src/test/java/br/nom/penha/bruno/treino/pact/consumer/tasks/service/pact/SalvaTarefaTest.java
+++ src/test/java/br/nom/penha/bruno/treino/pact/consumer/tasks/service/pact/SalvaTarefaTest.java
@@ -13,31 +13,37 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
-public class TasksConsumerContractTest {
+public class SalvaTarefaTest {
 
     @Rule
     public PactProviderRule mockProvider = new PactProviderRule("Tarefas", this);
 
     @Pact(consumer = "ConsumidorBasico")
     public RequestResponsePact criaPacto(PactDslWithProvider construtor){
-        DslPart corpo = new PactDslJsonBody()
-                            .numberType("id",1L)
+        DslPart corpoRequisicao = new PactDslJsonBody()
+                            .stringType("task", "Tarefa com data em String")
+                            .date("dueDate", "yyyy-MM-dd", new Date());
+
+        DslPart corpoResponse = new PactDslJsonBody()
+                            .numberType("id")
                             .stringType("task")
-                            .stringType("dueDate");
+                            .date("dueDate", "yyyy-MM-dd", new Date());
 
         return construtor
-                .given("Existe uma tarefa com o id = 1")
-                .uponReceiving("Quando vier uma tarefa #1")
-                    .path("/todo/1")
-                    .method("GET")
+                .uponReceiving("Salva a data de uma tarefa com uma string")
+                    .path("/todo")
+                    .method("POST")
+                    .body(corpoRequisicao)
                 .willRespondWith()
-                    .status(200)
-                    .body(corpo)
+                    .status(201)
+                    .body(corpoResponse)
                 .toPact();
     }
 
@@ -49,11 +55,13 @@ public class TasksConsumerContractTest {
         System.out.println("mockProvider.getUrl() >>>>>>>>>>> " + mockProvider.getUrl());
 
         // Executo a tarefa que quero testar
-        Task tarefa = consumidor.getTask(1L);
+        String dueDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        Task tarefa = consumidor.saveTask("Tarefa com data em String", dueDate);
 
         // Verifico o resultado
-        assertThat(tarefa.getId(), is(1L));
+        assertThat(tarefa.getId(), is(notNullValue()));
         assertThat(tarefa.getTask(), is(notNullValue()));
+
 
     }
 }
